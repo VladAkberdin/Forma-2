537fce1fd926a0f1610d165a78a8c836c61d7925
MaximNovoselov-wq <MaximNovoselov-wq@users.noreply.github.com>
2019-12-17 00:50:31 +0500

    Подключение таблицы к форме

--- Forma 2/Form1.cs
+++ Forma 2/Form1.cs
@@ -16,5 +16,20 @@ namespace Forma_2
         {
             InitializeComponent();
         }
+
+        private void TableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "database1DataSet.Table". При необходимости она может быть перемещена или удалена.
+            this.tableTableAdapter.Fill(this.database1DataSet.Table);
+
+        }
     }
 }
